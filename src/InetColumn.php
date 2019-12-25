de7ef1de30beb8aec9328f7f09cca2daf71c8dab
Shish <shish@shishnet.org>
2019-12-25 15:34:11 +0000

    emulate prepares for 'inet <var>'

--- src/InetColumn.php
+++ src/InetColumn.php
@@ -6,6 +6,7 @@ class InetColumn extends Column
     public function get_sql_filter(): string
     {
         $driver = $this->table->db->getAttribute(\PDO::ATTR_DRIVER_NAME);
+        $this->table->db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, True);
         switch ($driver) {
             case "pgsql":
                 return "({$this->name} <<= inet :{$this->name} OR {$this->name} >>= inet :{$this->name})";
