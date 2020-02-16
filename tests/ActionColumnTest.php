d43eacc28ed43e360ee5db0c0ba6f43ff4350f3d
Shish <shish@shishnet.org>
2020-02-16 21:56:05 +0000

    Add ActionColumnTest

---
+++ tests/ActionColumnTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class ActionColumnTest extends \PHPUnit\Framework\TestCase
+{
+    public $db = null;
+
+    public function setUp(): void
+    {
+        $this->db = create_mock_db();
+    }
+
+    public function test_like()
+    {
+        $t = new IPBanTable($this->db);
+        $t->inputs = ["r_all" => "on", "r_id" => "42"];
+        list($q, $a) = $t->get_filter();
+
+        $this->assertEquals("(1=1)", $q);
+        $this->assertEquals([], $a);
+    }
+}
