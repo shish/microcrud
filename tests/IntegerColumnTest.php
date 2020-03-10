246230d1f0017a7031f6956d5ca1a6289a531a3f
Shish <shish@shishnet.org>
2020-03-10 00:01:51 +0000

    test

---
+++ tests/IntegerColumnTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use \MicroCRUD\DateColumn;
+
+class IntegerColumnTest extends \PHPUnit\Framework\TestCase
+{
+    public $db;
+
+    public function setUp(): void
+    {
+        $this->db = create_mock_db();
+    }
+
+    public function test_exact_match()
+    {
+        $t = new IPBanTable($this->db);
+        $t->inputs = ["r_all" => "on", "r_banner_id" => "1"];
+        list($q, $a) = $t->get_filter();
+
+        $this->assertEquals("(banner_id = :banner_id)", $q);
+        $this->assertEquals(['banner_id' => '1'], $a);
+
+        $rows = $t->query();
+        $this->assertEquals("1.2.3.1", $rows[0]["ip"]);
+        $this->assertEquals(81, $t->count());
+    }
+}
